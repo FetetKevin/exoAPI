8d5e3edbe16b1f78400fffc0be3735e8294bc532
Fetet Kevin <fetetkevin@gmail.com>
2021-12-01 14:20:22 +0100

    ajout d'un boutton deroulant pour selectionner une ville

--- meteo.js
+++ meteo.js
@@ -3,7 +3,7 @@ var callBackGetSuccess = function(data) {
     var element = document.getElementById("resultat");
     var weather = data.weather[0].description;
     var bg = document.getElementById("bg");
-    element.innerHTML = "Il fait " + data.main.temp + "° a " + data.name + " !";
+    element.innerHTML = "It's " + data.main.temp + "° in " + data.name + " !";
     
 if (weather == "clear sky") {
         bg.className = ''
@@ -35,7 +35,7 @@ function getWeather() {
     
     var userQuery = document.getElementById("userQuery").value;
     var url = "https://api.openweathermap.org/data/2.5/weather?q="+userQuery+"&units=metric&appid=82ce023a35ef5ea9a9a216f786878f6a"
-
+    
     $.get(url, callBackGetSuccess).done(function() {
             //alert( "second success" );
         })
@@ -46,3 +46,39 @@ function getWeather() {
             //alert( "finished" );
         });
 }
+
+// Replace ./data.json with your JSON feed
+fetch('./cities.json')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    // Work with JSON data here
+    //console.log(data)
+    
+    function create_btn(){
+        let select = document.createElement('select');
+        select.className = 'city';
+        select.id= 'userQuery';
+        let elem = document.getElementById('cities');
+        elem.prepend(select);
+        for(i=0; i<data.length;i++){
+            let op = document.createElement('option');
+            op.setAttribute('value', `${data[i].name}`);
+            op.innerHTML = `${data[i].name}`;
+            select.appendChild(op);
+        }
+    }
+    function show(){
+        let btn = document.querySelector('.btn');
+        btn.addEventListener("click", function(){
+            let value = document.querySelector('.city').value;
+            document.getElementById('res').innerHTML = value;
+        })
+    }
+    create_btn();
+    show();
+  })
+  .catch((err) => {
+    // Do something for an error here
+  })
